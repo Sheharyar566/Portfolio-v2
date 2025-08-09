27a2a42246312dc7614cecd0ec981313afab3989
Sheharyar566 <Sheharyar566@gmail.com>
2025-08-10 00:59:49 +0500

    Started working on the project section

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,101 +15,6 @@ const sidebarBtn = document.querySelector("[data-sidebar-btn]");
 sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
 
 
-
-// testimonials variables
-const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
-const modalContainer = document.querySelector("[data-modal-container]");
-const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
-const overlay = document.querySelector("[data-overlay]");
-
-// modal variable
-const modalImg = document.querySelector("[data-modal-img]");
-const modalTitle = document.querySelector("[data-modal-title]");
-const modalText = document.querySelector("[data-modal-text]");
-
-// modal toggle function
-const testimonialsModalFunc = function () {
-  modalContainer.classList.toggle("active");
-  overlay.classList.toggle("active");
-}
-
-// add click event to all modal items
-for (let i = 0; i < testimonialsItem.length; i++) {
-
-  testimonialsItem[i].addEventListener("click", function () {
-
-    modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
-    modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
-    modalTitle.innerHTML = this.querySelector("[data-testimonials-title]").innerHTML;
-    modalText.innerHTML = this.querySelector("[data-testimonials-text]").innerHTML;
-
-    testimonialsModalFunc();
-
-  });
-
-}
-
-
-// custom select variables
-const select = document.querySelector("[data-select]");
-const selectItems = document.querySelectorAll("[data-select-item]");
-const selectValue = document.querySelector("[data-selecct-value]");
-const filterBtn = document.querySelectorAll("[data-filter-btn]");
-
-select.addEventListener("click", function () { elementToggleFunc(this); });
-
-// add event in all select items
-for (let i = 0; i < selectItems.length; i++) {
-  selectItems[i].addEventListener("click", function () {
-
-    let selectedValue = this.innerText.toLowerCase();
-    selectValue.innerText = this.innerText;
-    elementToggleFunc(select);
-    filterFunc(selectedValue);
-
-  });
-}
-
-// filter variables
-const filterItems = document.querySelectorAll("[data-filter-item]");
-
-const filterFunc = function (selectedValue) {
-
-  for (let i = 0; i < filterItems.length; i++) {
-
-    if (selectedValue === "all") {
-      filterItems[i].classList.add("active");
-    } else if (selectedValue === filterItems[i].dataset.category) {
-      filterItems[i].classList.add("active");
-    } else {
-      filterItems[i].classList.remove("active");
-    }
-
-  }
-
-}
-
-// add event in all filter button items for large screen
-let lastClickedBtn = filterBtn[0];
-
-for (let i = 0; i < filterBtn.length; i++) {
-
-  filterBtn[i].addEventListener("click", function () {
-
-    let selectedValue = this.innerText.toLowerCase();
-    selectValue.innerText = this.innerText;
-    filterFunc(selectedValue);
-
-    lastClickedBtn.classList.remove("active");
-    this.classList.add("active");
-    lastClickedBtn = this;
-
-  });
-
-}
-
-
-
 // contact form variables
 const form = document.querySelector("[data-form]");
 const formInputs = document.querySelectorAll("[data-form-input]");
@@ -197,5 +102,53 @@ for (let i = 0; i < skills.length; i++) {
     <img src="${skills[i].icon}" alt="${skills[i].name}" >
     <span>${skills[i].name}</span>
   </li>
+  `;
+}
+
+
+// Projects variables
+// Projects variables
+const projectsContainer = document.querySelector(".projects-list");
+const projects = [
+  {
+    title: "Crossbolt",
+    description: "Built the official website for CrossBolt Technologies, a design and development agency, using Next.js for a fast, SEO-friendly architecture, Tailwind CSS for a responsive and maintainable design system, and GSAP for smooth, high-performance animations. Focused on creating a clean, modern user experience that reflects the company’s brand while ensuring accessibility and optimal performance across devices.",
+    tags: ["NextJS", "ReactJS", "Tailwind CSS", "GSAP"],
+    imageUrl: "./assets/images/projects/crossbolt.png",
+    liveUrl: "https://github.com/sheharyarkhan/crossbolt",
+    sourceUrl: "https://github.com/sheharyarkhan/crossbolt"
+  },
+  {
+    title: "Crossbolt",
+    description: "A cross-platform mobile application for cross-border delivery.",
+    tags: ["React Native", "Flutter", "Node.js", "PostgreSQL", "MongoDB"],
+    imageUrl: "./assets/images/projects/crossbolt.png",
+    liveUrl: "https://github.com/sheharyarkhan/crossbolt",
+    sourceUrl: "https://github.com/sheharyarkhan/crossbolt"
+  }
+];
+
+for (let i = 0; i < projects.length; i++) {
+  const project = projects[i];
+
+  projectsContainer.innerHTML += `
+    <li class="project-item">
+      <a href="${project.liveUrl}" target="_blank">
+
+        <figure class="project-image">
+          <img src="${project.imageUrl}" alt="${project.title}" loading="lazy">
+        </figure>
+
+        <div class="project-content">
+          <h3 class="h3 project-title">${project.title}</h3>
+
+          <p class="project-description">${project.description}</p>
+
+          <ul class="project-tags">
+            ${project.tags.map(tag => `<li class="project-tag">${tag}</li>`).join('')}
+          </ul>
+        </div>
+      </a>
+    </li>
   `;
 }
